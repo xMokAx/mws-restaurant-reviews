fa667bc29c2169eada07965732f131656bdf4653
xMokAx <ahmedmokhtar11j@gmail.com>
2018-05-21 20:42:18 +0200

    fix: data source3

--- js/service_worker.js
+++ js/service_worker.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker
-    .register("/sw.js")
+    .register("./sw.js")
     .then(reg => {
       console.log("SW Registered", reg.scope);
     })
